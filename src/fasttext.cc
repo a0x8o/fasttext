9be7cc1e98171b333a52ccaebd7d679341808fd7
Alex Baretto <alex@cloudmondo.com>
2017-06-28 15:46:00 -0700

    2017-06-28:15:37:28

--- src/fasttext.cc
+++ src/fasttext.cc
@@ -25,7 +25,7 @@ namespace fasttext {
 
 FastText::FastText() : quant_(false) {}
 
-void FastText::getVector(Vector& vec, const std::string& word) {
+void FastText::getVector(Vector& vec, const std::string& word) const {
   const std::vector<int32_t>& ngrams = dict_->getNgrams(word);
   vec.zero();
   for (auto it = ngrams.begin(); it != ngrams.end(); ++it) {
@@ -639,6 +639,10 @@ void FastText::train(std::shared_ptr<Args> args) {
       saveOutput();
     }
   }
+}
+
+int FastText::getDimension() const {
+    return args_->dim;
 }
 
 }
